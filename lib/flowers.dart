77471b06f36d4558b51ea1d989e7d5b9697adfd1
sara-jr <salazar.ramirez.j@hotmail.com>
2024-08-19 21:54:38 -0600

    Modificada la interfaz para floes para el smartwatch

--- lib/flowers.dart
+++ lib/flowers.dart
@@ -1,93 +1,93 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart' as CarouselPkg;
 
 class FlowersGalery extends StatefulWidget {
   FlowersGalery({super.key});
-@override
-_FlowersGaleryState createState() => _FlowersGaleryState();
+  @override
+  _FlowersGaleryState createState() => _FlowersGaleryState();
 }
+
 class _FlowersGaleryState extends State<FlowersGalery> {
-final List<String> imgList = [
-  "assets/campanilla.jpg", 
-  "assets/cempazuchil.jpg",
-  "assets/dahlia.jpg",
-  "assets/magnolia.jpg",
-  "assets/nochebuena.jpg",
-];
+  final List<String> imgList = [
+    "assets/campanilla.jpg",
+    "assets/cempazuchil.jpg",
+    "assets/dahlia.jpg",
+    "assets/magnolia.jpg",
+    "assets/nochebuena.jpg",
+  ];
 
-int _current = 0;
-CarouselPkg.CarouselSliderController _controller = CarouselPkg.CarouselSliderController();
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-appBar: AppBar(
-title: Text('Mi Galeria'),
-),
-body: Column(
-children: [
-CarouselPkg.CarouselSlider(
-carouselController: _controller,
-options: CarouselPkg.CarouselOptions(
-height: 300.0,
-enlargeCenterPage: true,
-aspectRatio: 16 / 9,
-enableInfiniteScroll: true,
-viewportFraction: 0.8,
-onPageChanged: (index, reason) {
-setState(() {
-_current = index;
-});
-},
-),
-items: imgList
-.map((item) => Container(
-child: Center(
-child: Image.asset(
-item,
-fit: BoxFit.cover,
-height: 400,
-),
-),
-))
-.toList(),
-),
-SizedBox(height: 20),
-Row(
-mainAxisAlignment: MainAxisAlignment.center,
-children: [
-ElevatedButton(
-onPressed: () => _controller.previousPage(),
-child: Icon(Icons.arrow_back),
-),
-SizedBox(width: 20),
-ElevatedButton(
-onPressed: () => _controller.nextPage(),
-child: Icon(Icons.arrow_forward),
-),
-],
-),
-SizedBox(height: 20),
-Row(
-mainAxisAlignment: MainAxisAlignment.center,
-children: imgList.asMap().entries.map((entry) {
-return Container(
-width: 8.0,
-height: 8.0,
-margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
-decoration: BoxDecoration(
-shape: BoxShape.circle,
-color: _current == entry.key
-? Color.fromRGBO(0, 0, 0, 0.9)
-: Color.fromRGBO(0, 0, 0, 0.4),
-),
-);
-}).toList(),
-),
-],
-),
-);
+  int _current = 0;
+  CarouselPkg.CarouselSliderController _controller =
+      CarouselPkg.CarouselSliderController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flora'),
+      ),
+      body: Column(
+        children: [
+          CarouselPkg.CarouselSlider(
+            carouselController: _controller,
+            options: CarouselPkg.CarouselOptions(
+              height: 120.0,
+              enlargeCenterPage: true,
+              aspectRatio: 4 / 3,
+              enableInfiniteScroll: true,
+              viewportFraction: 0.8,
+              onPageChanged: (index, reason) {
+                setState(() {
+                  _current = index;
+                });
+              },
+            ),
+            items: imgList
+                .map((item) => Container(
+                      child: Center(
+                        child: Image.asset(
+                          item,
+                          fit: BoxFit.cover,
+                          height: 120.0,
+                        ),
+                      ),
+                    ))
+                .toList(),
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: () => _controller.previousPage(),
+                icon: const Icon(Icons.arrow_back),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: imgList.asMap().entries.map((entry) {
+                  return Container(
+                    width: 5.0,
+                    height: 5.0,
+                    margin:
+                        EdgeInsets.symmetric(vertical: 2.0, horizontal: 2.0),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: _current == entry.key
+                          ? Color.fromRGBO(0, 0, 0, 0.9)
+                          : Color.fromRGBO(0, 0, 0, 0.3),
+                    ),
+                  );
+                }).toList(),
+              ),
+              IconButton(
+                onPressed: () => _controller.nextPage(),
+                icon: const Icon(Icons.arrow_forward),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 }
-}
-
-
-
